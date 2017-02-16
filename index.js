2d5580368d4e39b24ae1d18c149d8b9625970bd8
Sergiu Suciu <sergiu.suciu@altom.ro>
2017-02-16 20:22:49 +0200

    connection test lol

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const Client = require('node-xmpp-client');
 
+const envOptions = {
+  lolId : process.env.LOL_ID + "@pvp.net/xiff",
+  lolPassword : "AIR_" + process.env.LOL_PASSWORD,
+  lolPort: process.env.LOL_PORT || 5223,
+  lolServer : process.env.LOL_SERVER || "chat.eun1.lol.riotgames.com",
+};
+
 const app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -19,10 +26,23 @@ app.get('/', function(req, res) {
 	res.send("Hi I am a chatbot");
 })
 
-
 app.listen(app.get('port'), function() {
 	console.log("running: port");
 })
+
+var client = new Client({
+  jid : envOptions.lolId,
+  password :  envOptions.lolPassword,
+  host : envOptions.lolServer,
+  port : envOptions.lolPort,
+  reconnect : true,
+  autostart : true,
+  legacySSL : true
+})
+
+client.on('stanza', function (stanza) {
+  console.log('Received stanza: ', stanza.toString());
+});
 
 
 
