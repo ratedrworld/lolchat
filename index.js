29340ec60826f83fa1fb8631ce6682fe17c0a97c
Sergiu Suciu <sergiu.suciu@altom.ro>
2017-02-16 21:32:04 +0200

    Fixed sending when null

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const Client = require('node-xmpp-client');
 
 
 const envOptions = {
-  lolId : process.env.LOL_ID + "@pvp.net/xiff",
-  lolPassword : "AIR_" + process.env.LOL_PASSWORD,
+  lolId: process.env.LOL_ID + "@pvp.net/xiff",
+  lolPassword: "AIR_" + process.env.LOL_PASSWORD,
   lolPort: process.env.LOL_PORT || 5223,
-  lolServer : process.env.LOL_SERVER || "chat.eun1.lol.riotgames.com",
+  lolServer: process.env.LOL_SERVER || "chat.eun1.lol.riotgames.com",
 };
 
 const app = express();
@@ -19,27 +19,27 @@ const app = express();
 app.set('port', (process.env.PORT || 5000));
 
 // Allows us to process the data
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // ROUTES
 
-app.get('/', function(req, res) {
-	res.send("Hi I am a chatbot");
+app.get('/', function (req, res) {
+  res.send("Hi I am a chatbot");
 })
 
-app.listen(app.get('port'), function() {
-	console.log("running: port");
+app.listen(app.get('port'), function () {
+  console.log("running: port");
 })
 
 var client = new Client({
-  jid : envOptions.lolId,
-  password :  envOptions.lolPassword,
-  host : envOptions.lolServer,
-  port : envOptions.lolPort,
-  reconnect : true,
-  autostart : true,
-  legacySSL : true
+  jid: envOptions.lolId,
+  password: envOptions.lolPassword,
+  host: envOptions.lolServer,
+  port: envOptions.lolPort,
+  reconnect: true,
+  autostart: true,
+  legacySSL: true
 })
 
 client.on('online', function () {
@@ -76,15 +76,17 @@ client.on('stanza', function (stanza) {
   if (stanza.is('message') && stanza.attrs.type === 'chat') {
     var message = stanza.getChildText('body');
     console.log(stanza.getChildText('body'));
-    var reply = new Client.Stanza('message', {
-      to: stanza.attrs.from,
-      from: stanza.attrs.to,
-      type: 'chat'
-    })
-    reply.c('body').t('I can echo' + message);
-    setTimeout(function () {
-      client.send(reply)
-    }, 321)
+    if (message && message.length > 0) {
+      var reply = new Client.Stanza('message', {
+        to: stanza.attrs.from,
+        from: stanza.attrs.to,
+        type: 'chat'
+      })
+      reply.c('body').t('I can echo' + message);
+      setTimeout(function () {
+        client.send(reply)
+      }, 321)
+    }
   }
 })
 
